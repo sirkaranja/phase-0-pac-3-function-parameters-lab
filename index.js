ccb24aac47426ea544a0f4bb7b3a19277e596517
Daniel Karanja <sirkaranja12@gmail.com>
2023-03-08 16:41:56 +0300

    done with javascript(paraemter)

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function introduction(name){
-    console.log(`Hi, my name is ${name}`);
+    return `Hi, my name is ${name}.`;
 }
-introduction("Aki");
 
+//second test
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
 
+// 3rd test with default language
+function introductionWithLanguageOptional(name, language= "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+// 4rd test with default language
+
+function introductionWithLanguageOptional(name, language= "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+    
